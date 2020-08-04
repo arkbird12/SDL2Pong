fbbe00f3664eca335df68635e33402e6ca893d06
arkbird12 <rishi.r.thakkar@gmail.com>
2020-08-03 18:12:22 -0700

    More boilerplate. Initial screen rendering test

--- Pong.cpp
+++ Pong.cpp
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
 {
     SDL_Window *win = NULL;
     SDL_Renderer *render = NULL;
+    SDL_Event event;
 
     static bool isRunning = true;
 
@@ -15,7 +16,7 @@ int main(int argc, char const *argv[])
         std::cerr << "Error intitializing SDL" << std::endl;
         return -1;
     }
-    
+
     win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
     if(win == NULL) {
         std::cerr << "Error creating window: " << SDL_GetError() << std::endl;
@@ -24,16 +25,26 @@ int main(int argc, char const *argv[])
     
     render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(render == NULL) {
-        std::cerr << "Error creating renderer: " << SDL_GetError() << endl;
+        std::cerr << "Error creating renderer: " << SDL_GetError() << std::endl;
     }
     
+    SDL_SetRenderDrawColor(render, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(render);
+    SDL_RenderPresent(render);
+
     //core game loop begins     
     while(isRunning) {
-        
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+            if(event.type == SDL_KEYDOWN) {
+                isRunning = false;
+            }
+        }
     }
 
-    SDL_Delay(10000);
-    SDL_DestroyRenderer();
+    SDL_DestroyRenderer(render);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
